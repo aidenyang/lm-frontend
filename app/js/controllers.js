e1385a4f8f0c2605812bcfea7bfeab7e866668c9
Aiden Yang <alyang250@gmail.com>
2014-07-12 15:09:08 -0700

    first commit

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,59 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('MyCtrl1', ['$scope', 'DataFactory', function($scope, DataFactory) {
+
+  	// hide insert form
+  	$scope.showForm = false;
+  	$scope.showButton = true;
+
+  	$scope.status;
+  	$scope.links; 
+  	getAllLinks();
+
+  	function getAllLinks() {
+  		DataFactory.getAllLinks()
+  		.success(function (data) {
+  			$scope.links = data.items;
+  			$scope.status = "Fetched all links.";
+  		}).
+  		error(function() {
+  			$scope.status = "Unable to fetch links. Please try again later.";
+  		});
+  	};
+
+  	// $scope.getLinkById = function(id) {
+  	// 	DataFactory.getLinkById(id)
+  	// 	.success(function (data)) {
+
+  	// 	};
+  	// };
+
+  	$scope.insertLink = function(author, title, url) {
+  		DataFactory.insertLink(author, title, url)
+  		.success(function (data){
+  			$scope.status = "Successfully added the link.";
+  		})
+  		.error(function (data) {
+  			$scope.status = "Sorry, we couldn't add your link at this time. Please try again later.";
+  		});
+  	}
+
+  	$scope.deleteLink = function(id) {
+  		DataFactory.deleteLink(id) 
+  		.success(function (data) {
+  			for (var i = 0; i < $scope.links.length; i++) {
+  				if ($scope.links[i].id == id) {
+  					$scope.links.splice(i, 1);
+  					break;
+  				}
+  			}
+  			$scope.status = "Successfully deleted link with id " + id;
+  		})
+  		.error(function (data) {
+  			$scope.status = "Could not delete link with id " + id;
+  		});
+  	};
 
   }])
   .controller('MyCtrl2', ['$scope', function($scope) {
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,24 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  factory('DataFactory', ['$http', function($http){
+
+  	var DataFactory = {};
+
+  	DataFactory.getAllLinks = function() {
+  		return $http.get('http://link-manager.herokuapp.com/links');
+  	};
+
+  	DataFactory.getLinkById = function(id) {
+  		return $http.get('http://link-manager.herokuapp.com/links/' + id);
+  	};
+
+  	DataFactory.deleteLink = function(id) {
+  		return $http.delete('http://link-manager.herokuapp.com/links/' + id);
+  	}
+
+  	DataFactory.insertLink = function() {
+
+  	};
+  	return DataFactory;
+  }]);
